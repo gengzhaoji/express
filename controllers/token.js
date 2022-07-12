d3b8dae83fa396ed3e1b09c785b0060b026c034b
耿朝继 <1317993594@qq.comn>
2022-07-12 11:10:11 +0800

    token过期时间24小时

--- controllers/token.js
+++ controllers/token.js
@@ -6,7 +6,7 @@ const setToken = function (user_name, user_id) {
   return new Promise((resolve, reject) => {
     //expiresln 设置token过期的时间
     //{ user_name: user_name, user_id: user_id } 传入需要解析的值（ 一般为用户名，用户id 等）
-    const token = jwt.sign({ user_name: user_name, user_id: user_id }, secret, {expiresIn: 60});
+    const token = jwt.sign({ user_name: user_name, user_id: user_id }, secret, {expiresIn: 60 * 60 * 24});
     resolve(token);
   });
 };
