49caf7713b9216bad7f9214bc409bd5b6cd7c292
gengzhaoji <1317993594@qq.com>
2025-09-02 17:47:42 +0800

    提交

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,21 @@ const {
   queryService,
   loginService,
 } = require("../controllers/index/indexCtrl");
-/**
- * 测试路径
- */
+
+
 router.get("/", (req, res) => {
   queryService(req, res);
 });
 router.get("/login", (req, res) => {
   loginService(req, res);
+});
+
+// 单工通讯，服务端实时主动推送，适合大屏的项目，实时性要求高
+router.get("/sse", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  setInterval(() => {
+    res.write(`data: ${new Date().toLocaleTimeString()}\n\n`);
+  }, 1000);
 });
 
 module.exports = router;
