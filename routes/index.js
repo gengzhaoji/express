6f6e8e36cec25bad547ea3be1057c0e8ccee5b5e
gengzhaoji <1317993594@qq.com>
2025-09-02 17:52:07 +0800

    调整

--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,15 @@ router.get("/login", (req, res) => {
 });
 
 // 单工通讯，服务端实时主动推送，适合大屏的项目，实时性要求高
+/**
+ * 前端通过EventSource对象来接收服务器推送的数据，
+ * const sse = new EventSource("http://loaclhost:3000/sse");
+ * sse.onmessage = (e) => { console.log(e.data)}
+ */
 router.get("/sse", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   setInterval(() => {
+    res.write(`event: message\n`) // 事件名称，默认为message
     res.write(`data: ${new Date().toLocaleTimeString()}\n\n`);
   }, 1000);
 });
